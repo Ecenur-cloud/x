7237b23b85a9dc86e255ae77178205b42cb44bf0
Ecenur-cloud <Ecenruzn@gmail.com>
2024-08-05 17:05:44 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+
+  const [Numara, setCount] = useState(0);
+
+
+  const Y = () => {
+    setCount(Numara + 1);
+  }
+  const N = () => {
+    if (Numara > 0) {
+      setCount(Numara - 1);
+    }
+  }
+
+
+  return (
+    <div class="click" >
+
+      <div><button class="a" onClick={Y}>Artır +</button></div>
+      <div class="count">{Numara}</div>
+      <div><button class="b" onClick={N}>Azalt -</button></div>
+
+    </div>
+  );
+}
+
+export default App;
